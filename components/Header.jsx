469881a391c28d9ca778cbc2d0f6279dc0cce78c
Guillaume <guigui.mareschal@gmail.com>
2022-08-03 16:41:15 +0200

    theme color

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
 
   return (
     <>
-      <meta name="theme-color" content="black" />
+      <meta name="theme-color" content="#016cec" />
       <div className="container mx-auto px-10 mb-8">
         <div className="border-b w-full inline-block border-blue-400 py-8">
           <div className="md:float-left block">
